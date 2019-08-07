e75fafca72bd809d4e4162ef540d16286a0dae7c
Uehara Yuuku <e175732@ie.u-ryukyu.ac.jp>
2019-08-08 02:42:19 +0900

    Update PokemonMultiLabelClassification.py

--- workspace/src/32/PokemonMultiLabelClassification.py
+++ workspace/src/32/PokemonMultiLabelClassification.py
@@ -8,54 +8,23 @@ import numpy as np
 from PIL import Image
 import glob
 import sys
-
-type_dict = {
-    "Normal":0, "Fire":1, "Water":2, "Electric":3, "Grass":4, "Ice":5,
-    "Fighting":6, "Poison":7, "Ground":8, "Flying":9, "Psychic":10, "Bug":11,
-    "Rock":12, "Ghost":13, "Dragon":14, "Dark":15,"Steel":16, "Fairy":17
-}
-
-data_dir = '/workspace/data/'
-
-def load_images():
-    images_path = data_dir + 'images2/*'
-    image_paths = glob.glob(images_path)
-    image_paths.sort()
-    images_length = len(image_paths)
-    raw_images = []
-    for image_path,i in zip(image_paths, range(images_length)):
-        img = Image.open(image_path)
-        raw_images.append(np.array(img))
-        print(f'\rloading images ... {i}/{images_length}', end='')
-    print(f'\rloading images ... {images_length}/{images_length} done')
-    raw_images = np.array(raw_images)
-    return raw_images
+import dataset
 
 
-def load_types():
-    types_file_path = data_dir + 'Pokemon.csv'
+# 画像の形状情報
+IMG_ROWS = 64
+IMG_COLS = 64
+IMG_CHANNELS = 3
+IMG_CLASSES = 18
 
-    print(f'\rloading types data ... ', end='')
-    types_df = pd.read_csv(types_file_path, sep=',')
-    types_df.drop_duplicates(subset='Number', inplace=True)
-    types_df.reset_index(inplace=True, drop=True)
+# fit parameters
+BATCH_SIZE = 32
+EPOCHS = 10
 
-    # Type2がない場合,Type1で補完する
-    df = types_df.copy(deep=True)
-    ind = df[df['Type2'].isnull()]['Type2'].index
-    df.iloc[ind, 3] = df.iloc[ind, 2]
-
-    df_1 = df["Type1"][:801]
-    df_1 = df_1.map(type_dict)
-    type1 = df_1.values
-
-    df_2 = df["Type2"][:801]
-    df_2 = df_2.map(type_dict)
-    type2 = df_2.values
-
-    print(f'\rloading types data ... done')
-
-    return type1, type2
+# 学習済みモデルの保存ファイル
+MODEL_FILE_PATH = './pokemon_multi_label_classification_cnn_model.json'
+INIT_WEIGHT_FILE_PATH = './pokemon_multi_label_classification_cnn_init_weight.hdf5'
+BEST_WEIGHT_FILE_PATH = './pokemon_multi_label_classification_cnn_best_weight.hdf5'
 
 
 def data_augmentation(images, labels):
@@ -103,13 +72,13 @@ def data_shuffle(images,types):
     return np.array(x),np.array(y)
 
 
-def generate_model(img_rows,img_cols,img_channels,nb_classes):
+def generate_model():
     model = Sequential()
     model.add(Conv2D(32, (5, 5),
                      padding='same',
                      activation='relu',
                      kernel_regularizer=regularizers.l2(0.001),
-                     input_shape=(img_rows,img_cols,img_channels)))
+                     input_shape=(IMG_ROWS,IMG_COLS,IMG_CHANNELS)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
     model.add(Conv2D(64, (5, 5),
@@ -123,7 +92,7 @@ def generate_model(img_rows,img_cols,img_channels,nb_classes):
                     activation='relu',
                     kernel_regularizer=regularizers.l2(0.001)))
     model.add(Dropout(0.5))
-    model.add(Dense(nb_classes,
+    model.add(Dense(IMG_CLASSES,
                     kernel_regularizer=regularizers.l2(0.001),
                     activation='sigmoid'))
 
@@ -150,20 +119,10 @@ def generator(x, y, batch_size=32):
 
 
 def fit(raw_images, type1, type2):
-    # 画像の形状情報
-    img_rows = 64
-    img_cols = 64
-    img_channels = 3
-    nb_classes = 18
-
-    # fit parameter
-    batch_size = 32
-    epochs = 2
-
     # 前処理
     images = raw_images.astype('float32') / 255.0
-    type1_c = utils.to_categorical(type1, nb_classes)
-    type2_c = utils.to_categorical(type2, nb_classes)
+    type1_c = utils.to_categorical(type1, IMG_CLASSES)
+    type2_c = utils.to_categorical(type2, IMG_CLASSES)
     types = type1_c + type2_c
     types = np.where(types > 1., 1., types)
 
@@ -177,16 +136,16 @@ def fit(raw_images, type1, type2):
     # w_data = pd.concat([types_df['Type1'][:750],types_df['Type2'][:750]]).value_counts(normalize=True,sort=True)
     # class_weight = pd.Series(w_data.values[0] / w_data.values, index=w_data.index.map(type_dict)).to_dict()
 
-    model = generate_model(img_rows, img_cols, img_channels, nb_classes)
+    model = generate_model()
 
     # JSON形式でモデルを保存
     json_string = model.to_json()
-    open('./pokemon_cnn.json', 'w').write(json_string)
+    open(MODEL_FILE_PATH, 'w').write(json_string)
     #　初期ウエイトの保存
-    model.save_weights('./pokemon_cnn_init_weight.hdf5', overwrite=True)
+    model.save_weights(INIT_WEIGHT_FILE_PATH, overwrite=True)
 
     # バリデーションロスが下がれば、エポックごとにモデルを保存
-    cp_cb = ModelCheckpoint(filepath='./pokemon_cnn_best_weight1.hdf5',
+    cp_cb = ModelCheckpoint(filepath=BEST_WEIGHT_FILE_PATH,
                             monitor='val_loss',
                             verbose=1,
                             save_best_only=True,
@@ -202,38 +161,37 @@ def fit(raw_images, type1, type2):
 
     model.fit_generator(generator=generator(x=img_train,
                                             y=type_train,
-                                            batch_size=batch_size),
-                        steps_per_epoch=len(img_train)//batch_size,
+                                            batch_size=BATCH_SIZE),
+                        steps_per_epoch=len(img_train)//BATCH_SIZE,
                         validation_data=generator(x=img_valid,
                                                   y=type_valid,
-                                                  batch_size=batch_size),
-                        validation_steps=len(img_valid)//batch_size,
+                                                  batch_size=BATCH_SIZE),
+                        validation_steps=len(img_valid)//BATCH_SIZE,
 #                         class_weight=class_weight,
                         verbose=1,
-                        epochs=epochs,
+                        epochs=EPOCHS,
                         callbacks=[history,cp_cb,es_cb])
 
 
 def predict(image):
-    # 画像の形状情報
-    img_rows = 64
-    img_cols = 64
-    img_channels = 3
-    nb_classes = 18
-
     # 学習済みモデルとパラメータの呼び出し
-    json_string = open('./pokemon_cnn.json', 'r').read()
+    json_string = open(MODEL_FILE_PATH, 'r').read()
     model = model_from_json(json_string)
-    model.load_weights('./pokemon_cnn_best_weight1.hdf5')
+    model.load_weights(BEST_WEIGHT_FILE_PATH)
 
     img =  np.array([image]).astype('float32') / 255.0
 
+    type_dict = dataset.type_dict
     type_labels = {v: k for k, v in type_dict.items()}
 
     # 予測
     pred=model.predict(img, batch_size=1)
 
-    print(type_labels[np.argmax(pred[0])])
+    # 予測結果表示
+    pred_first_type = type_labels[np.argmax(pred[0])]
+    pred[0][np.argmax(pred[0])] = 0
+    pred_seconds_type = type_labels[np.argmax(pred[0])]
+    print(pred_first_type, pred_seconds_type)
 
 
 if __name__ == '__main__':
@@ -247,15 +205,28 @@ if __name__ == '__main__':
     if len(args) >= 2:
         if args[1] == 'fit':
             # Load data.
-            raw_images = load_images()
-            type1, type2 = load_types()
+            raw_images, (type1, type2) = dataset.load_data()
+
+            # モデルに合う様に変換
+            images = []
+            for _img in raw_images:
+                img = Image.fromarray(_img, 'RGBA')
+                img = img.convert('RGB')
+                img = img.resize((64,64))
+                images.append(np.array(img))
+            images = np.array(images)
+
             # Run fit.
-            fit(raw_images, type1, type2)
+            fit(images, type1, type2)
+
         elif args[1] == 'predict':
             # Load image.
             image_path = args[2]
             img = Image.open(image_path)
+            img = img.convert('RGB')
+            img = img.resize((64,64))
             image = np.array(img)
+
             # Run predict.
             predict(image)
     else:
